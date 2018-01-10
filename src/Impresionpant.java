90d38bb34864afb413d6bab08f3d225e73ad9521
aythamipr <aythamipr@hotmail.com>
2018-01-10 06:44:49 +0000

    Commit inicial

---
+++ src/Impresionpant.java
@@ -0,0 +1,14 @@
+
+public class Impresionpant {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Bienvenido al sistema");
+		System.out.println("Hoy es lunes");
+		System.out.println("Son las 08:00");
+		System.out.println("Tiene 8 correos sin leer");
+		
+	}
+
+}
